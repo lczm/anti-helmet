f8230b57649033835e991d1e2e287e18e23af15c
lczm <zemingchua@gmail.com>
2022-01-09 12:55:13 +0800

    zeming day 16 part 2 i dunno man

--- zeming/day16/main.go
+++ zeming/day16/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
 	"strconv"
 	"strings"
 )
 
-type Op int64
+type Op int
 
 const (
 	Sum Op = iota
@@ -21,9 +22,10 @@ const (
 	Default
 )
 
-var versionSum int64
-var stack []Op = make([]Op, 0)
-var literalStack []int64 = make([]int64, 0)
+var versionSum int
+
+// var stack []Op = make([]Op, 0)
+// var literalStack []int = make([]int, 0)
 var table map[string]string = map[string]string{
 
 	"0": "0000",
@@ -52,11 +54,11 @@ func hexToBin(s string, table map[string]string) string {
 	return n
 }
 
-func p(s string) int {
+func p(s string) (int, int) {
 	return pHeader(s)
 }
 
-func pHeader(s string) int {
+func pHeader(s string) (int, int) {
 	// Eat the version and type
 	version := s[:3]
 	typeID := s[3:6]
@@ -65,31 +67,40 @@ func pHeader(s string) int {
 	typeIDInt, _ := strconv.ParseInt(typeID, 2, 64)
 
 	// Increment the version sum used for part 1
-	versionSum += versionInt
+	versionSum += int(versionInt)
 
 	switch typeIDInt {
 	case 4: // Literal evaluation
-		return pLiteral(s[6:]) + 6
+		a, b := pLiteral(s[6:])
+		return a + 6, b
 	case 0: // sum packets
-		return pOperator(s[6:], Sum) + 6
+		a, b := pOperator(s[6:], Sum)
+		return a + 6, b
 	case 1: // product packets
-		return pOperator(s[6:], Product) + 6
+		a, b := pOperator(s[6:], Product)
+		return a + 6, b
 	case 2: // minimum packets
-		return pOperator(s[6:], Min) + 6
+		a, b := pOperator(s[6:], Min)
+		return a + 6, b
 	case 3: // maximum packets
-		return pOperator(s[6:], Max) + 6
+		a, b := pOperator(s[6:], Max)
+		return a + 6, b
 	case 5: // greater than packets
-		return pOperator(s[6:], Gt) + 6
+		a, b := pOperator(s[6:], Gt)
+		return a + 6, b
 	case 6: // less than packets
-		return pOperator(s[6:], Lt) + 6
+		a, b := pOperator(s[6:], Lt)
+		return a + 6, b
 	case 7: // equal to packets
-		return pOperator(s[6:], Eq) + 6
+		a, b := pOperator(s[6:], Eq)
+		return a + 6, b
 	default:
-		return pOperator(s[6:], Default) + 6
+		a, b := pOperator(s[6:], Default)
+		return a + 6, b
 	}
 }
 
-func pLiteral(s string) int {
+func pLiteral(s string) (int, int) {
 	ip := 0
 	builder := ""
 
@@ -105,74 +116,122 @@ func pLiteral(s string) int {
 	}
 
 	literal, _ := strconv.ParseInt(builder, 2, 64)
+
 	// _ = literal
+	// literalStack = append(literalStack, literal)
+	// stack = append(stack, Literal)
 
-	literalStack = append(literalStack, literal)
-	stack = append(stack, Literal)
-
-	return ip
+	return ip, int(literal)
 }
 
-func pushOp(o Op) {
-	stack = append(stack, o)
-}
+// func pushOp(o Op) {
+// 	stack = append(stack, o)
+// }
 
-func pushLiteral(literal int64) {
-	literalStack = append(literalStack, literal)
-}
+// func pushLiteral(literal int) {
+// 	literalStack = append(literalStack, literal)
+// }
 
-func pOperator(s string, o Op) int {
+func pOperator(s string, op Op) (int, int) {
 	ip := 0
 	count := 0
+	val := 0
+	values := make([]int, 0)
 	if s[ip] == '0' { // next 15-bits
 		l := s[1:16]
 		lInt, _ := strconv.ParseInt(l, 2, 64)
 		count = 0
-		for int64(count) != lInt {
-			count += p(s[count+16:])
+		for int(count) != int(lInt) {
+			a, b := p(s[count+16:])
+			count += a
+			values = append(values, b)
 		}
-		pushOp(o)
-		return count + 16
+		switch op {
+		case Sum:
+			for _, v := range values {
+				val += v
+			}
+		case Product:
+			var product int = 1
+			for _, v := range values {
+				product *= v
+			}
+			val = product
+		case Min:
+			val = min(values)
+		case Max:
+			val = max(values)
+		case Gt:
+			val = gt(values[0], values[1])
+		case Lt:
+			val = lt(values[0], values[1])
+		case Eq:
+			val = eq(values[0], values[1])
+		}
+		return count + 16, val
 	} else if s[ip] == '1' { // next 11-bits
 		l := s[1:12]
 		lInt, _ := strconv.ParseInt(l, 2, 64)
 		count = 0
-		for i := 0; int64(i) < lInt; i++ {
-			count += p(s[count+12:])
+		for i := 0; int(i) < int(lInt); i++ {
+			a, b := p(s[count+12:])
+			count += a
+			values = append(values, b)
 		}
-		pushOp(o)
-		return count + 12
+		switch op {
+		case Sum:
+			for _, v := range values {
+				val += v
+			}
+		case Product:
+			var product int = 1
+			for _, v := range values {
+				product *= v
+			}
+			val = product
+		case Min:
+			val = min(values)
+		case Max:
+			val = max(values)
+		case Gt:
+			val = gt(values[0], values[1])
+		case Lt:
+			val = lt(values[0], values[1])
+		case Eq:
+			val = eq(values[0], values[1])
+		}
+		return count + 12, val
 	} else {
 		panic("error parsing operator")
 	}
-	return count
+	return count, val
 }
 
-func popStack() Op {
-	op := stack[len(stack)-1]
-	stack = stack[:len(stack)-1]
-	return op
-}
+// func popStack() Op {
+// 	op := stack[len(stack)-1]
+// 	stack = stack[:len(stack)-1]
+// 	return op
+// }
 
-func peekStack() Op {
-	return stack[len(stack)-1]
-}
+// func peekStack() Op {
+// 	return stack[len(stack)-1]
+// }
 
-func canPeekStack() bool {
-	if len(stack) >= 1 {
-		return true
-	}
-	return false
-}
+// func canPeekStack() bool {
+// 	if len(stack) >= 1 {
+// 		return true
+// 	}
+// 	return false
+// }
 
-func popLiteralStack() int64 {
-	literal := literalStack[len(literalStack)-1]
-	literalStack = literalStack[:len(literalStack)-1]
-	return literal
-}
+// func popLiteralStack() int {
+// 	literal := literalStack[len(literalStack)-1]
+// 	literalStack = literalStack[:len(literalStack)-1]
+// 	return literal
+// }
 
-func min(a []int64) int64 {
-	var m int64 = 9999999999
+func min(a []int) int {
+	var m int = math.MaxInt
 	for _, v := range a {
 		if v < m {
 			m = v
@@ -181,8 +240,8 @@ func min(a []int64) int64 {
 	return m
 }
 
-func max(a []int64) int64 {
-	var m int64 = 0
+func max(a []int) int {
+	var m int = 0
 	for _, v := range a {
 		if v > m {
 			m = v
@@ -191,21 +250,21 @@ func max(a []int64) int64 {
 	return m
 }
 
-func gt(a, b int64) int64 {
+func gt(a, b int) int {
 	if a > b {
 		return 1
 	}
 	return 0
 }
 
-func lt(a, b int64) int64 {
+func lt(a, b int) int {
 	if a < b {
 		return 1
 	}
 	return 0
 }
 
-func eq(a, b int64) int64 {
+func eq(a, b int) int {
 	if a == b {
 		return 1
 	}
@@ -213,47 +272,47 @@ func eq(a, b int64) int64 {
 }
 
 // Uses stack & literalStack
-func eval(op Op) int64 {
-	switch op {
-	case Sum:
-		var sum int64 = 0
-		for canPeekStack() && peekStack() == Literal {
-			sum += eval(popStack())
-		}
-		return sum
-	case Product:
-		var product int64 = 1
-		for canPeekStack() && peekStack() == Literal {
-			product *= eval(popStack())
-		}
-		return product
-	case Min:
-		val := make([]int64, 0)
-		for canPeekStack() && peekStack() == Literal {
-			val = append(val, eval(popStack()))
-		}
-		return min(val)
-	case Max:
-		val := make([]int64, 0)
-		for canPeekStack() && peekStack() == Literal {
-			val = append(val, eval(popStack()))
-		}
-		return max(val)
-	case Gt:
-		lit2, lit1 := eval(popStack()), eval(popStack())
-		return gt(lit1, lit2)
-	case Lt:
-		lit2, lit1 := eval(popStack()), eval(popStack())
-		return lt(lit1, lit2)
-	case Eq:
-		lit2, lit1 := eval(popStack()), eval(popStack())
-		return eq(lit1, lit2)
-	case Literal:
-		lit := popLiteralStack()
-		return lit
-	}
-	return 0
-}
+// func eval(op Op) int {
+// 	switch op {
+// 	case Sum:
+// 		var sum int = 0
+// 		for canPeekStack() && peekStack() == Literal {
+// 			sum += eval(popStack())
+// 		}
+// 		return sum
+// 	case Product:
+// 		var product int = 1
+// 		for canPeekStack() && peekStack() == Literal {
+// 			product *= eval(popStack())
+// 		}
+// 		return product
+// 	case Min:
+// 		val := make([]int, 0)
+// 		for canPeekStack() && peekStack() == Literal {
+// 			val = append(val, eval(popStack()))
+// 		}
+// 		return min(val)
+// 	case Max:
+// 		val := make([]int, 0)
+// 		for canPeekStack() && peekStack() == Literal {
+// 			val = append(val, eval(popStack()))
+// 		}
+// 		return max(val)
+// 	case Gt:
+// 		lit2, lit1 := eval(popStack()), eval(popStack())
+// 		return gt(lit1, lit2)
+// 	case Lt:
+// 		lit2, lit1 := eval(popStack()), eval(popStack())
+// 		return lt(lit1, lit2)
+// 	case Eq:
+// 		lit2, lit1 := eval(popStack()), eval(popStack())
+// 		return eq(lit1, lit2)
+// 	case Literal:
+// 		lit := popLiteralStack()
+// 		return lit
+// 	}
+// 	return 0
+// }
 
 func main() {
 	// part1
@@ -270,14 +329,14 @@ func main() {
 
 		line := lines[0]
 		bin := hexToBin(line, table)
-		s := p(bin)
-		r := eval(popStack())
+		s, r := p(bin)
+		// r := eval(popStack())
 
 		fmt.Println("#", in, "versionSum:", versionSum, s, len(bin), r)
 
 		// reset all
 		versionSum = 0
-		stack = make([]Op, 0)
-		literalStack = make([]int64, 0)
+		// stack = make([]Op, 0)
+		// literalStack = make([]int, 0)
 	}
 }
